e2890dfd4f61ede6999ca032c6547a58ff0b0f09
hamza <hamzaensi@gmail.com>
2024-10-11 14:10:56 +0100

    Mise à jours de debug

--- exercice1.js
+++ exercice1.js
@@ -1,13 +1,23 @@
+/**
+ * Utilisez console
+ * 1- Afficher la vaeur de a 
+ * 2- Afficher la valeur de c
+ * 3- Afficher le tableau
+ * 4- Corriger les erreurs
+ */
 
 
 function sayHello() {
+
     a = 5;
     
-    b = 6;
+    b = "6";
 
-    c = a + b;
-    
-              
+    c = a + b; // 11
+
+    tableau =[5,9,3]
+            x = tableau[3]
+            console.log(x)  
             const h1 = document.getElementById("hello";
             const h2 = document.getElementById("php");
             h1.innerHTML = "Hello EveryBody !"
