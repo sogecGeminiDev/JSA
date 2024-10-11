3583f0c164b4f11e3dd0616307c5ce0cc541cfb9
hamza <hamzaensi@gmail.com>
2024-10-11 14:57:19 +0100

    fin rappel

---
+++ exercice4.js
@@ -0,0 +1,58 @@
+/***
+ Voici 6 questions à choix multiples (QCM) qui vous aideront à comprendre quand utiliser une fonction ou un objet en JavaScript, selon la définition donnée.
+
+QCM 1 :
+Vous devez calculer le carré d’un nombre plusieurs fois dans votre programme.
+Que devez-vous utiliser ?
+
+A. Une fonction
+B. Un objet
+
+Réponse : ?
+
+QCM 2 :
+Vous devez stocker les informations sur un utilisateur (nom, âge, email) 
+pour pouvoir y accéder et les manipuler ensemble. Que devez-vous utiliser ?
+
+A. Une fonction
+B. Un objet
+
+Réponse : ?
+
+QCM 3 :
+Vous avez un ensemble d'instructions qui doit être exécuté chaque fois qu'un bouton 
+est cliqué dans l'interface utilisateur. Que devez-vous utiliser ?
+
+A. Une fonction
+B. Un objet
+
+Réponse : ?
+
+QCM 4 :
+Vous devez gérer un panier d'achat avec plusieurs produits, 
+chacun ayant des propriétés comme le nom, le prix et la quantité. 
+Que devez-vous utiliser ?
+
+A. Une fonction
+B. Un objet
+
+Réponse : ?
+
+QCM 5 :
+Vous voulez créer un programme qui génère une salutation personnalisée (ex. "Bonjour, Alice") en fonction du nom de la personne. Que devez-vous utiliser ?
+
+A. Une fonction
+B. Un objet
+
+Réponse : ?
+
+QCM 6 :
+Vous avez un personnage dans un jeu avec des propriétés comme la santé, la vitesse et l'inventaire. Vous devez manipuler ces propriétés. Que devez-vous utiliser ?
+
+A. Une fonction
+B. Un objet
+
+Réponse : ?
+ 
+ 
+ */
