d9830f57faef9f82f615825a909bbd8797ce63ea
hamza <hamzaensi@gmail.com>
2024-10-11 14:27:47 +0100

    exercice1+2

---
+++ exercice1.js
@@ -0,0 +1,27 @@
+/***
+ * 
+ *    Créez un programme JavaScript qui :
+ * 2- Définit une fonction addition qui prend deux nombres et retourne leur somme.
+ * 3- Crée une variable maFonction et assigne-lui la fonction addition.
+ * 4- Passe cette variable (qui contient la fonction) à une autre fonction
+ *    executerFonction qui exécutera cette fonction avec deux arguments donnés.
+ * 5- Utilise executerFonction pour afficher la somme de deux nombres, 
+ *    par exemple, 5 et 3.
+ */
+
+
+// Définition de la fonction addition
+  
+   // # your code here #
+ 
+// Assigner la fonction à une variable
+  
+    // # your code here #
+
+// Définir une fonction qui prend une autre fonction en argument
+  
+     // # your code here #
+
+// Appeler executerFonction avec maFonction, et afficher le résultat
+
+      // # your code here #
---
+++ exercice2.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ * Créez un programme JavaScript qui :
+ * 1- Définit un objet personne avec des propriétés nom et age,
+ *    et une méthode parler qui affiche une phrase avec le nom 
+ *    de la personne.
+ * 2- Assigne cet objet à une variable monObjet.
+ * 3- Passe cet objet comme argument à une fonction 
+ *    afficherInfos qui va afficher le nom et l'âge de 
+ *    la personne, puis appeler la méthode parler de l'objet.
+ */
+
+// Définition de l'objet personne
+
+ // # your code here #
+
+// Assigner l'objet à une variable
+
+ // # your code here #
+
+// Définir une fonction qui prend un objet en argument
+
+ // # your code here #
+
+// Appeler afficherInfos avec monObjet
+
+ // # your code here #
