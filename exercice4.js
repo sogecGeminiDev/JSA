b45bdbd1678771206c1823f2125ab0e79abf70d5
hamza <hamzaensi@gmail.com>
2024-10-14 09:45:59 +0100

    ok

---
+++ exercice4.js
@@ -0,0 +1,35 @@
+/**
+
+Créer une classe avec des propriétés privées :
+
+Créez une classe appelée Utilisateur avec les caractéristiques 
+suivantes :
+
+Une propriété privée #motDePasse pour stocker le mot de passe 
+de l'utilisateur.
+Un méthode publique changerMotDePasse(nouveauMotDePasse) 
+qui permet de mettre à jour le mot de passe.
+Une méthode publique afficherInfo() qui affiche le nom de 
+l'utilisateur sans révéler le mot de passe.
+Astuce : Utilisez le préfixe # pour rendre #motDePasse privé.
+
+Tester la classe :
+
+Créez une instance de Utilisateur avec un nom et un mot de 
+passe initial.
+Essayez de changer le mot de passe en utilisant la méthode 
+changerMotDePasse().
+Essayez d'accéder directement à #motDePasse et notez ce qui 
+se passe.
+
+
+Questions :
+Que se passe-t-il lorsque vous essayez d'accéder directement 
+à #motDePasse ?
+Pourquoi est-il important d'utiliser des propriétés 
+privées dans certains cas ?
+Pouvez-vous toujours modifier le mot de passe ? Comment ?
+
+
+ */
+
