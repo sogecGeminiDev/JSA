549fb7b408dfd516aa7b167308ae2ee7cbef6a80
Julien GEORGET <jgeorget@sogec-marketing.fr>
2024-10-16 09:58:07 +0200

    validation formulaire

--- formulaire/validation.js
+++ formulaire/validation.js
@@ -7,7 +7,7 @@ function validateForm() {
     error.textContent = "";
 
     // Validation du nom d'utilisateur (lettres et chiffres, entre 3 et 15 caractères)
-    let usernameRegex = ;
+    let usernameRegex =/^[0-9A-Za-z]{3,15}$/ ;
     if (!usernameRegex.test(username)) {
         error.textContent = "Nom d'utilisateur invalide.";
         return false;
@@ -15,22 +15,22 @@ function validateForm() {
 
     // Validation de l'email forme générale
     // puis un email se termine par google.fr
-    let emailRegex = ;
+    let emailRegex = /\S+@\S+\.\S+/;
     if (!emailRegex.test(email)) {
         error.textContent = "Adresse e-mail invalide.";
         return false;
     }
 
-    // Validation du mot de passe (au moins 8 caractères, majuscule, minuscule, chiffre, caractère spécial)
-    let passwordRegex = ;
+    // // Validation du mot de passe (au moins 8 caractères, majuscule, minuscule, chiffre, caractère spécial)
+    let passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[\W_]){8,}/;
     if (!passwordRegex.test(password)) {
         error.textContent = "Mot de passe invalide.";
         return false;
     }
 
-    // 1- Validation du numéro de téléphone (format 123-456-7890)
-    // 2- il faut que le numero commence par 07 
-    let phoneRegex = ;
+    // // 1- Validation du numéro de téléphone (format 123-456-7890)
+    // // 2- il faut que le numero commence par 07 
+    let phoneRegex = /^07-\d{3}-\d{4}$/;
     if (!phoneRegex.test(phone)) {
         error.textContent = "Numéro de téléphone invalide.";
         return false;
